0d61723e73e87cdc0e6f5c11b0573c958d472b89
Cole Perry <E096752@wv.gov>
2021-02-02 14:06:00 -0500

    Searching works for all files

--- Controllers/Family_Leave_Forms_Controller.cs
+++ Controllers/Family_Leave_Forms_Controller.cs
@@ -11,19 +11,36 @@ namespace CovidAppV5.Controllers
     public class Family_Leave_Forms_Controller : Controller
     {
         // GET: Family_Leave_Forms_
-        public ActionResult Index()
+        public ActionResult Index(string currentFilter, string searchString)
         {
             string path = Server.MapPath("~/Family_Leave_Docs/");
             string[] fileEntries = Directory.GetFiles(path);
             var docs = new List<string>();
-            foreach (string fileName in fileEntries)
+            if (!String.IsNullOrEmpty(searchString)) //If there is a search string
             {
-                string result = fileName.Substring(fileName.LastIndexOf(@"\") + 1);
-                docs.Add(result);
+                foreach (string fileName in fileEntries)
+                {
+                    if (fileName.Contains(searchString))
+                    {
+                        string result = fileName.Substring(fileName.LastIndexOf(@"\") + 1);
+                        docs.Add(result);
+                    }
+                }
+                string[] arrayOfDocs = docs.ToArray();
+                ViewData["arrayOfDocs"] = arrayOfDocs; //Must include the array using both methods
+                return View(arrayOfDocs);
             }
-            string[] arrayOfDocs = docs.ToArray();
-            ViewData["arrayOfDocs"] = arrayOfDocs; //Must include the array using both methods
-            return View(arrayOfDocs);
+            else //If there is not a search string
+            {
+                foreach (string fileName in fileEntries)
+                {
+                    string result = fileName.Substring(fileName.LastIndexOf(@"\") + 1);
+                    docs.Add(result);
+                }
+                string[] arrayOfDocs = docs.ToArray();
+                ViewData["arrayOfDocs"] = arrayOfDocs; //Must include the array using both methods
+                return View(arrayOfDocs);
+            }
         }
 
         // GET: Family_Leave_Forms_/Create
