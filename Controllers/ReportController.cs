295e4ceb54d409fdebe3be85a30d84bdc0618075
Cole Perry <E096752@wv.gov>
2021-02-11 15:58:51 -0500

    Report joining kinda working

---
+++ Controllers/ReportController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data;
+using System.Data.Entity;
+using System.IO;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using CovidAppV5.Models;
+using CovidAppV5.ViewModel;
+
+namespace CovidAppV5.Controllers
+{
+    public class ReportController : Controller
+    {
+        private Covid19Entities db = new Covid19Entities();
+        // GET: Report
+        public ActionResult Index()
+        {
+            List<COVIDvm> VMlist = new List<COVIDvm>(); // to hold list of forms
+            var covidQuery = (from form in db.Case_Log
+                              where form.Name == "Cole"
+                              join eForm in db.Emergency_Leave on form.Name equals eForm.Name
+                              select new {form.Name, form.Phone1, form.Phone2, eForm.OrgNumber, eForm.Annual, eForm.PaidSick}).ToList();
+           // select form).ToList();
+
+            foreach (var item in covidQuery)
+            {
+                COVIDvm objcvm = new COVIDvm(); // ViewModel
+                objcvm.Name = item.Name;
+                objcvm.Phone1 = item.Phone1;
+                objcvm.Phone2 = item.Phone2;
+                objcvm.OrgNumber = item.OrgNumber;
+                objcvm.Division_District = item.Annual.ToString();
+                objcvm.OrgNumber = item.PaidSick.ToString();
+                VMlist.Add(objcvm);
+            }
+            return View(VMlist);
+        }
+    }
+}
---
+++ ViewModel/COVIDvm.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace CovidAppV5.ViewModel
+{
+    public class COVIDvm
+    {
+        //All from questionaires
+
+        public string Name { get; set; }
+        public string Phone1 { get; set; }
+        public string Phone2 { get; set; }
+        [Column("Division/District")]
+        public string Division_District { get; set; }
+        public string OrgNumber { get; set; }
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:d}")]
+        public System.DateTime DateOfTest { get; set; }
+        public string DateOfExposure { get; set; }
+
+        //All from Emergency Leave
+        public bool UnableToTelework { get; set; }
+        public bool CaringForMinor { get; set; }
+        public bool SpecialCircumstance { get; set; }
+        public double Annual { get; set; }
+        public double PaidSick { get; set; }
+        public double EmergencyPaidSick { get; set; }
+        public double Unpaid { get; set; }
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:d}")]
+        public System.DateTime LeaveFrom { get; set; }
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:d}")]
+        public System.DateTime LeaveTo { get; set; }
+
+        //All from Family Leave
+        public bool QuarantineOrder { get; set; }
+        public bool AdviseToSelfQuarantine { get; set; }
+        public bool Symptoms { get; set; }
+        public bool CaringForPerson { get; set; }
+        public bool ChildCareUnavailable { get; set; }
+        public bool SimilarConditions { get; set; }
+    }
+}
