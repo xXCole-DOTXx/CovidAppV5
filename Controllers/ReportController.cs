0bfc412716e4f0d9dcfedab2f15dd23008f83e48
Cole Perry <E096752@wv.gov>
2021-02-12 14:02:04 -0500

    Slight changes to report

--- Controllers/ReportController.cs
+++ Controllers/ReportController.cs
@@ -19,18 +19,18 @@ namespace CovidAppV5.Controllers
         public ActionResult Index(string Name)
         {
             List<COVIDvm> VMlist = new List<COVIDvm>(); // to hold list of forms
+
+
             var covidQuery = (from form in db.Case_Log
                               where form.Name == Name
                               join eForm in db.Emergency_Leave on form.Name equals eForm.Name
-                              select new {form.Name, form.Phone1, form.Phone2, eForm.OrgNumber, eForm.UnableToTelework, eForm.CaringForMinor}).ToList();
-           // select form).ToList();
+                              select new {form.Name, form.Phone1, eForm.OrgNumber, eForm.UnableToTelework, eForm.CaringForMinor}).ToList();
 
             foreach (var item in covidQuery)
             {
                 COVIDvm objcvm = new COVIDvm(); // ViewModel
                 objcvm.Name = item.Name;
                 objcvm.Phone1 = item.Phone1;
-                objcvm.Phone2 = item.Phone2;
                 objcvm.OrgNumber = item.OrgNumber;
                 objcvm.UnableToTelework = item.UnableToTelework;
                 objcvm.CaringForMinor = item.CaringForMinor;
